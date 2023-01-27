06b8aef129830b25a5ab91a6a781f2d4a38c7b78
Franco Galfre <francogalfre2007@gmail.com>
2023-01-26 22:06:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// HTML Selector
+const details = document.querySelector('.details');
+const imgContainer = document.querySelector('.img-container');
+const userBtn = document.getElementById('get-user-btn');
+
+// Function Fetch GetUser()
+const getUser = () => {
+  fetch('https://random-data-api.com/api/v2/users?response_type=json')
+    .then(res => res.json())
+    .then(data => {
+      // Image Avatar
+      imgContainer.innerHTML = `<img src=${data.avatar}/>`
+      // Details
+      details.innerHTML = `
+        <h2> ${data.first_name} ${data.last_name} </h2>
+        <h3> ${data.employment.title} </h3>
+        <h4><i class="fa-solid fa-location-dot"></i> ${data.address.city} </h4>
+      `;
+
+      // Random Color
+      let randomColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+      document.documentElement.style.setProperty("--theme-color", randomColor);
+
+    })
+    .catch(err => console.warn(err))
+}
+
+userBtn.addEventListener("click", getUser)
